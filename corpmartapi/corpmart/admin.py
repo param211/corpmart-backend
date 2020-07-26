da8766a4614d674b92d0416917475ed4bede6818
Param <paramchauhan21@gmail.com>
2020-07-26 16:33:08 +0530

    Remove extra apps from Admin

--- corpmartapi/corpmart/admin.py
+++ corpmartapi/corpmart/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from .models import User, OneTimePassword, Business, Balancesheet, BalancesheetPayment, Blog, Testimonial, \
     ContactRequest
+from django.apps import apps
+from rest_framework.authtoken.models import Token
+
+
+# De-register all models from other apps
+for app_config in apps.get_app_configs():
+    for model in app_config.get_models():
+        if admin.site.is_registered(model):
+            admin.site.unregister(model)
+
+
+# admin.site.unregister(Token)
 
 
 # Register your models here.
--- corpmartapi/settings.py
+++ corpmartapi/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corpmartapi.corpmart',
     'rest_framework',
     'rest_framework.authtoken',
+    'corpmartapi.corpmart',
 ]
 
 MIDDLEWARE = [
