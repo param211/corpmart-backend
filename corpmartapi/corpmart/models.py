4eef53afd7f4e93629b9c9ea2b8b5b26c20cfc65
Param <paramchauhan21@gmail.com>
2020-07-29 19:53:19 +0530

    Add name to viewhistory

--- corpmartapi/corpmart/models.py
+++ corpmartapi/corpmart/models.py
@@ -263,3 +263,6 @@ class ViewHistory(models.Model):
     business = models.ForeignKey(Business, related_name='viewhistory', on_delete=models.CASCADE)
     viewed_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='viewhistory', on_delete=models.CASCADE)
     viewed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'ViewHistory'
