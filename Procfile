63bb6a20bc0c83acbd854161d5dede18021f8d73
param211 <paramchauhan21@gmail.com>
2021-08-13 22:54:25 +0530

    Changes for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn corpmartapi.wsgi --log-file -
--- corpmartapi/settings.py
+++ corpmartapi/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -232,12 +232,21 @@ EMAIL_HOST_PASSWORD = 'SG.y6VpIrwoSiib13pVxUd1GA.J5VfynmNcUuyCWBNBhG78xSql1v7vIC
 
 
 # Heroku: Update database configuration from $DATABASE_URL.
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
-# psycopg2-binary==2.7.7
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
+
+psycopg2-binary==2.7.7
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
